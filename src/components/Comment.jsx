c0aee453d8e92f910ea4df0daba6dcd573b7c094
Fábio Marins <82336114+marinsfabio@users.noreply.github.com>
2022-11-23 21:15:48 -0300

    atualização css

---
+++ src/components/Comment.jsx
@@ -0,0 +1,34 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src='https://avatars.githubusercontent.com/u/82336114?s=400&u=18929db694d44ee0bd8856c359c36fc0f55b0224&v=4' />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Fabio Marins</strong>
+              <time>cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentario'>
+              <Trash size={20}  />
+            </button>
+          </header>
+
+          <p>Muito bom Devon,  parabens!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,4 +1,5 @@
 import styles from './Post.module.css'
+import { Comment } from './Comment'
 
 export function Post() {
   return (
@@ -32,6 +33,12 @@ export function Post() {
 
         <button type='submit'>comentar</button>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     
     </article>
   )
